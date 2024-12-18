a458867e997f4d3e794118ff44e2a70d08b9a719
Suroj Dangol <surojsmiling@gmail.com>
2024-12-18 13:02:13 +0545

    chore: Add robots and sitemap integrations, update site URL, and remove unused invitation card page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
+import robots from 'astro-robots';
+
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
 	integrations: [
 		tailwind(),
@@ -12,5 +16,8 @@ export default defineConfig({
 				mdi: ['*'],
 			},
 		}),
+		robots(),
+		sitemap(),
 	],
+    site:'https://surojd.com.np',
 });
